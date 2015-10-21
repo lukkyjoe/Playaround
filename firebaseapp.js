00251526381ed3946d4fd1cb239124769a5677b8
lukkyjoe <joseph.sc.luk@gmail.com>
2015-10-21 08:53:16 -0500

    trying firebase standalone code

---
+++ firebaseapp.js
@@ -0,0 +1,9 @@
+var app = angular.module("sampleApp", ["firebase"]);
+app.controller("SampleCtrl", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://sizzling-torch-7126.firebaseio.com");
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
+});
